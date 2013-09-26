db25b1548dd222c0564ae991b1ca32f5c0efd3ff
snowhow <bernd@snowhow.info>
2013-09-26 10:28:30 +0200

    remove unused libs in import

--- src/android/RecorderService.java
+++ src/android/RecorderService.java
@@ -25,9 +25,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.RandomAccessFile;
